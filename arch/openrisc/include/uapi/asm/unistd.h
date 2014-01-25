ccfe17892fde0e9032eee4de3519cdcb2f0282f5
Christian Svensson <blue@cmd.nu>
2014-01-25 15:42:36 +0000

    openrisc: Add new atomic operations to or1k_atomic
    OpenRISC does not offer any way to do atomic operations in user space.
    In order to provide atomic operations we need to implement these
    operations in kernel space.
    
    These operations disables preemption by disabling interrupts while
    executing their critical sections.
    
    The following operations has been added:
     SWAP:     Atomically swap the values in pointers 1 and 2.
     CMPXCHG:  Writes new to *mem if *mem == old. Returns old *mem.
     XCHG:     Store NEW in *MEM and return the old value.
     ADD:      Add VAL to *MEM and return the old value of *MEM.
     DECPOS:   Decrement *MEM if it is > 0, and return the old value.
     AND:      Atomically *mem &= mask and return the old value of *mem.
     OR:       Atomically *mem |= mask and return the old value of *mem.
     UMAX:     If *mem < val, set *mem = max. Returns old value of *mem.
     UMIN:     If *mem > val, set *mem = min. Returns old value of *mem.
    
    These operations have been chosen to satisfy the need of atomic.h in
    glibc. While it is possible to only use CMPXCHG, the glibc
    implementation of the others use a do-while loop to retry the operation
    until it succeeds. In order to offer linear performance for these
    operations they are therefore implemented in the syscall.
    
    Signed-off-by: Christian Svensson <blue@cmd.nu>

--- arch/openrisc/include/uapi/asm/unistd.h
+++ arch/openrisc/include/uapi/asm/unistd.h
@@ -25,5 +25,46 @@
 
 #include <asm-generic/unistd.h>
 
+/*
+ * uint32 sys_or1k_atomic(uint32 op, ...)
+ *
+ * uint32 sys_or1k_atomic(SWAP, void *ptr1, void *ptr2)
+ *   Atomically swap the values in pointers 1 and 2.
+ *
+ * uint32 sys_or1k_atomic(CMPXCHG, uint32 *mem, uint32 old, uint32 new)
+ *   Writes new to *mem if *mem == old. Returns old *mem.
+ *
+ * uint32 sys_or1k_atomic(XCHG, uint32 *mem, uint32 new)
+ *   Store NEW in *MEM and return the old value.
+ *
+ * uint32 sys_or1k_atomic(ADD, uint32 *mem, uint32 val)
+ *   Add VAL to *MEM and return the old value of *MEM.
+ *
+ * uint32 sys_or1k_atomic(DECPOS, uint32 *mem)
+ *   Decrement *MEM if it is > 0, and return the old value.
+ *
+ * uint32 sys_or1k_atomic(AND, uint32 *mem, uint32 mask)
+ *   Atomically *mem &= mask and return the old value of *mem.
+ *
+ * uint32 sys_or1k_atomic(OR, uint32 *mem, uint32 mask)
+ *   Atomically *mem |= mask and return the old value of *mem.
+ *
+ * uint32 sys_or1k_atomic(UMAX, uint32 *mem, uint32 max)
+ *   If *mem < val, set *mem = max. Returns old value of *mem.
+ *
+ * uint32 sys_or1k_atomic(UMIN, uint32 *mem, uint32 min)
+ *   If *mem > val, set *mem = min. Returns old value of *mem.
+ */
+#define SYS_OR1K_ATOMIC_SWAP	1
+#define SYS_OR1K_ATOMIC_CMPXCHG	2
+#define SYS_OR1K_ATOMIC_XCHG	3
+#define SYS_OR1K_ATOMIC_ADD	4
+#define SYS_OR1K_ATOMIC_DECPOS	5
+#define SYS_OR1K_ATOMIC_AND	6
+#define SYS_OR1K_ATOMIC_OR	7
+#define SYS_OR1K_ATOMIC_UMAX	8
+#define SYS_OR1K_ATOMIC_UMIN	9
+#define SYS_OR1K_ATOMIC_COUNT	10
+
 #define __NR_or1k_atomic __NR_arch_specific_syscall
 __SYSCALL(__NR_or1k_atomic, sys_or1k_atomic)
